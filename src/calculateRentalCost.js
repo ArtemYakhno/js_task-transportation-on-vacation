e6014923fe5bb725dff14aa1a61609512d7142b5
Artem Yakhno <artemen23093@gmail.com>
2025-09-17 14:15:07 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,23 @@
  *
  * @return {number}
  */
+
+const AUTO_PRICE_PER_DAY = 40;
+const BIG_SALE = 50;
+const SMALL_SALE = 20;
+
 function calculateRentalCost(days) {
-  // write code here
+  let sale = 0;
+
+  if (days >= 3) {
+    sale = SMALL_SALE;
+  }
+
+  if (days >= 7) {
+    sale = BIG_SALE;
+  }
+
+  return days * AUTO_PRICE_PER_DAY - sale;
 }
 
 module.exports = calculateRentalCost;
